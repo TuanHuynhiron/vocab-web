ddc32e07be095e3e005d32a58f034e18aa44baa0
TuanHuynhiron <160412536+TuanHuynhiron@users.noreply.github.com>
2026-03-11 13:46:52 +0700

    Refactor server port configuration and log message

--- server.js
+++ server.js
@@ -369,6 +369,8 @@ app.post("/delete/:index", (req, res) => {
 
     res.sendStatus(200);
 });
-app.listen(3000, () => {
-    console.log("Server chạy tại http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
